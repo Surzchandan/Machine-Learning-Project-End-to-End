2c1908de9fd796e11e0188e6e8edbfc4e3f71379
Suraj C <surzchandan@gmail.com>
2022-11-16 14:45:07 +0530

    Logger file updated

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from housing.logger import logging
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("We are testing logging module")
     return "starting Machine learning Project"
 
 
