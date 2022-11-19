a84321f9b8ce99fcac81bae6ef0ecb2fc8673c12
Suraj C <surzchandan@gmail.com>
2022-11-19 17:48:11 +0530

    data transformation updated

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -1,52 +1,61 @@
-from collections import namedtuple
-from datetime import datetime
-import uuid
 from housing.config.configuration import Configuartion
-from housing.logger import logging, get_log_file_name
+from housing.logger import logging
 from housing.exception import HousingException
-from threading import Thread
-from typing import List
 
-from multiprocessing import Process
-from housing.entity.artifact_entity import ModelPusherArtifact, DataIngestionArtifact, ModelEvaluationArtifact
-from housing.entity.artifact_entity import DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact
-from housing.entity.config_entity import DataIngestionConfig, ModelEvaluationConfig
+from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from housing.entity.config_entity import DataIngestionConfig
 from housing.component.data_ingestion import DataIngestion
 from housing.component.data_validation import DataValidation
-from housing.component.data_transformation import DataTransformation
-from housing.component.model_trainer import ModelTrainer
-from housing.component.model_evaluation import ModelEvaluation
-from housing.component.model_pusher import ModelPusher
-import os, sys
-from collections import namedtuple
-from datetime import datetime
-import pandas as pd
-from housing.constant import EXPERIMENT_DIR_NAME, EXPERIMENT_FILE_NAME
+import os,sys
 
-Experiment = namedtuple("Experiment", ["experiment_id", "initialization_timestamp", "artifact_time_stamp",
-                                       "running_status", "start_time", "stop_time", "execution_time", "message",
-                                       "experiment_file_path", "accuracy", "is_model_accepted"])
+class Pipeline:
 
-
-
-
-
-class Pipeline(Thread):
-    experiment: Experiment = Experiment(*([None] * 11))
-    experiment_file_path = None
-
-    def __init__(self, config: Configuartion ) -> None:
+    def __init__(self,config: Configuartion = Configuartion()) -> None:
         try:
-            os.makedirs(config.training_pipeline_config.artifact_dir, exist_ok=True)
-            Pipeline.experiment_file_path=os.path.join(config.training_pipeline_config.artifact_dir,EXPERIMENT_DIR_NAME, EXPERIMENT_FILE_NAME)
-            super().__init__(daemon=False, name="pipeline")
-            self.config = config
-        except Exception as e:
-            raise HousingException(e, sys) from e
+            self.config=config
 
-    def start_data_ingestion(self) -> DataIngestionArtifact:
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
+    def start_data_ingestion(self)->DataIngestionArtifact:
         try:
             data_ingestion = DataIngestion(data_ingestion_config=self.config.get_data_ingestion_config())
             return data_ingestion.initiate_data_ingestion()
         except Exception as e:
-            raise HousingException(e, sys) from e
+            raise HousingException(e,sys) from e    
+
+
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact) \
+        -> DataValidationArtifact :
+        try:
+            data_validation =  DataValidation(data_validation_config=self.config.get_data_validation_config(),
+                                              data_ingestion_artifact=data_ingestion_artifact
+            )
+            return data_validation.initiate_data_validation()
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
+    def start_data_transformation(self):
+        pass
+
+    def start_model_trainer(self):
+        pass
+
+    def start_model_evaluation(self):
+        pass
+
+    def start_model_pusher(self):
+        pass
+
+    def run_pipeline(self):
+        try:
+            #data ingestion
+
+            data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+
+            
+
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
